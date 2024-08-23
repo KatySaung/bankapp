2851c2e3a82663507bfaba64eb476c8e3cf6d9d5
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2024-08-22 22:43:46 -0400

    transactions repository created

--- src/main/java/com/example/final_project/repository/TransactionRepository.java
+++ src/main/java/com/example/final_project/repository/TransactionRepository.java
@@ -1,4 +1,11 @@
 package com.example.final_project.repository;
 
-public interface TransactionRepository {
+import com.example.final_project.entities.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    // public List<Transaction> findByAccountId(Long accountId);
+    // public List<Transaction> findByUserId(Long userId);
+    // public List<Transaction> findByTimestampBetween(LocalDateTime startDate, LocalDateTime endDate);
+    // public List<Transaction> findByTransactionType(String transactionType);
 }
