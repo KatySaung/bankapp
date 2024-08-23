43d61a61144d187c9d7b80abd5b85c141a39c28c
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2024-08-22 22:57:37 -0400

    transactions repository and service inplemented

--- src/main/java/com/example/final_project/repository/TransactionRepository.java
+++ src/main/java/com/example/final_project/repository/TransactionRepository.java
@@ -3,9 +3,13 @@ package com.example.final_project.repository;
 import com.example.final_project.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // public List<Transaction> findByAccountId(Long accountId);
-    // public List<Transaction> findByUserId(Long userId);
-    // public List<Transaction> findByTimestampBetween(LocalDateTime startDate, LocalDateTime endDate);
-    // public List<Transaction> findByTransactionType(String transactionType);
+    List<Transaction> findByFromAccountId(Long accountId);
+     public List<Transaction> findByAccountId(Long accountId);
+     public List<Transaction> findByUserId(Long userId);
+     public List<Transaction> findByTransactionType(String transactionType);
+    List<Transaction> findByTimestampBetween(String startDate, String endDate);
 }
--- src/main/java/com/example/final_project/service/TransactionService.java
+++ src/main/java/com/example/final_project/service/TransactionService.java
@@ -1,4 +1,59 @@
 package com.example.final_project.service;
 
+import com.example.final_project.entities.Transaction;
+import com.example.final_project.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public List<Transaction> createTransaction(Transaction transaction) {
+        return transactionRepository.saveAll(List.of(transaction));
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction getTransactionById(Long id) {
+        return transactionRepository.findById(id).orElse(null);
+    }
+
+    public Transaction addTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Transaction updateTransaction(Transaction updatedTransaction) {
+        return transactionRepository.save(updatedTransaction);
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
+    }
+
+    public List<Transaction> getTransactionsByAccountId(Long accountId) {
+        return transactionRepository.findByFromAccountId(accountId);
+    }
+
+    public List<Transaction> getTransactionsByDate(String startDate, String endDate) {
+        return transactionRepository.findByTimestampBetween(startDate, endDate);
+    }
+
+    public List<Transaction> getTransactionsByUserId(Long userId) {
+        return transactionRepository.findByUserId(userId);
+
+    }
+
+    public List<Transaction> getTransactionsByType(String transactionType) {
+        return transactionRepository.findByTransactionType(transactionType);
+    }
+
+    public List<Transaction> findByAccountId(Long accountId) {
+        return transactionRepository.findByAccountId(accountId);
+    }
 }
