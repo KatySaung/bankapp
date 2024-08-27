f23c0fc63deafb4e52266c29b035562026bb9963
Katherine Yanan Saung <katysaung@gmail.com>
2024-08-27 00:04:23 -0400

    unknown previous changes in CreateAccountRequestDTO, need to commit inorder to merge with updates from kat branch

--- src/main/java/com/example/final_project/dto/CreateAccountRequestDto.java
+++ src/main/java/com/example/final_project/dto/CreateAccountRequestDto.java
@@ -1,10 +1,5 @@
 package com.example.final_project.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 public class CreateAccountRequestDTO {
     private Integer number;
     private Integer routNum;
@@ -17,5 +12,59 @@ public class CreateAccountRequestDTO {
     public CreateAccountRequestDTO(int i, int i1, String accountName, Double openingBalance, Integer[] integers, Double openingBalance1, Integer customerId) {
     }
 
-}
+    public Double getBalance() {
+        return balance;
+    }
 
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Double getOpeningBalance() {
+        return openingBalance;
+    }
+
+    public void setOpeningBalance(Double openingBalance) {
+        this.openingBalance = openingBalance;
+    }
+
+    public Integer getRoutNum() {
+        return routNum;
+    }
+
+    public void setRoutNum(Integer routNum) {
+        this.routNum = routNum;
+    }
+
+    public Integer[] getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Integer[] transactions) {
+        this.transactions = transactions;
+    }
+}
