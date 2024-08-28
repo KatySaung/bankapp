eb4c9e9740c7b4ed2ae2cab7769558123c545376
Katherine Yanan Saung <katysaung@gmail.com>
2024-08-27 23:37:37 -0400

    updated testing for TransactionServiceTest

--- src/main/java/com/example/final_project/dto/TransactionRequestDTO.java
+++ src/main/java/com/example/final_project/dto/TransactionRequestDTO.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 @Setter
 @Getter
 public class TransactionRequestDTO {
-
     private Integer type;
     private Integer fromAccount;
     private Integer fromAccountSortCode;
@@ -14,4 +13,60 @@ public class TransactionRequestDTO {
     private Integer toAccountSortCode;
     private Double amount;
 
+    public TransactionRequestDTO(Integer type, Integer fromAccount, Integer fromAccountSortCode, Integer toAccount, Integer toAccountSortCode, Double amount) {
+        this.type = type;
+        this.fromAccount = fromAccount;
+        this.fromAccountSortCode = fromAccountSortCode;
+        this.toAccount = toAccount;
+        this.toAccountSortCode = toAccountSortCode;
+        this.amount = amount;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(Integer fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public Integer getFromAccountSortCode() {
+        return fromAccountSortCode;
+    }
+
+    public void setFromAccountSortCode(Integer fromAccountSortCode) {
+        this.fromAccountSortCode = fromAccountSortCode;
+    }
+
+    public Integer getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(Integer toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public Integer getToAccountSortCode() {
+        return toAccountSortCode;
+    }
+
+    public void setToAccountSortCode(Integer toAccountSortCode) {
+        this.toAccountSortCode = toAccountSortCode;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
