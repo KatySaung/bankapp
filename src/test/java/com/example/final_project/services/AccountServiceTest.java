42b87c3660ad6542e1c54c2e660ddb827a8e85ff
Katherine Yanan Saung <katysaung@gmail.com>
2024-09-01 23:20:49 -0400

    completed test for method testCreateAccount in Account Services Test

--- src/test/java/com/example/final_project/services/AccountServiceTest.java
+++ src/test/java/com/example/final_project/services/AccountServiceTest.java
@@ -38,29 +38,30 @@ public class AccountServiceTest {
     @Mock
     private CustomerRepository customerRepository;
 
-    private CustomerDTO customerDTO;
-    private CreateAccountRequestDTO requestDto;
-    private CreateAccountResponseDTO responseDTO;
-
     @InjectMocks
     private AccountServiceImplm accountServiceImplm;
 
     private Customer customer;
+    private CustomerDTO customerDTO;
     private Account account;
+    private CreateAccountRequestDTO requestDto;
+    private CreateAccountResponseDTO responseDTO;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         MockitoAnnotations.openMocks(this);
 
         customer = new Customer("Tony Stark");
         customer.setCustomerId(1L); //need customer id because it is in customer class. without this here, will get error
-        account = new Account(12345678,1234,"Tony Stark","CHECKING",1000.00,customer);
+        account = new Account(12345678, 1234, "Tony Stark", "CHECKING", 1000.00, customer);
+        requestDto = new CreateAccountRequestDTO(1L,"Tony Stark", 1000.00);
+        responseDTO = new CreateAccountResponseDTO(12345678,1234,"Tony Stark",1000.00,1L);
 
     }
 
     @Test
-    public void testFindAccountByNumber(){
-        Account account = new Account(12345678,1234,"Steve Checking Acct","CHECKING",5000.00, customer);
+    public void testFindAccountByNumber() {
+//        Account account = new Account(12345678, 1234, "Steve Checking Acct", "CHECKING", 5000.00, customer);
         when(accountRepository.findByAccountNumber(12345678)).thenReturn(Optional.of(account));
 
         AccountDTO accountDTO2 = accountServiceImplm.getAccountByNumber(12345678);
@@ -68,24 +69,35 @@ public class AccountServiceTest {
         assertNotNull(accountDTO2);
         assertEquals(12345678, accountDTO2.number());
         assertEquals(1234, accountDTO2.sortCode());
-        assertEquals("Steve Checking Acct", accountDTO2.name());
-        assertEquals(5000.00, accountDTO2.balance());
-        assertEquals(customer.getCustomerId(),accountDTO2.customerId());
+        assertEquals("Tony Stark", accountDTO2.name());
+        assertEquals(1000.00, accountDTO2.balance());
+        assertEquals(customer.getCustomerId(), accountDTO2.customerId());
         verify(accountRepository).findByAccountNumber(12345678);
 
     }
-//    @Test
-//    public void testCreateAccount(){
-//        Account account = new Account(12345678,1234,"Steve Checking Acct","CHECKING",5000.00, customer);
-//        when(customerRepository.save(any())).thenReturn(customer);
-//
-//        AccountDTO accountDTO2 = accountServiceImplm.createAccount();
-//
-//        when(customerRepository.save(any(Customer.class))).thenReturn(account.getCustomer());
-//
-//        assertEquals(12345678 , accountDTO2.number());
-////        assertEquals(customer.getCustomerId(),accountDTO2.customerId());
-////        verify(customerRepository).save(any(Customer.class));
+
+    @Test
+    public void testCreateAccount() {
+        //accountResponseDTO variable is for the actual result in the testCreateAccount() method
+        //responseDTO from setUp() method will still be for the expected result
+
+        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
+
+        //need to create constructor with variable account1 with hard coded value because accountResponseDTO will generate a new account number each time.
+        Account account1 = new Account(12345678, 1234,"Tony Stark","CHECKING", 1000.00, customer);
+        when(accountRepository.save(any(Account.class))).thenReturn(account);
+
+        CreateAccountResponseDTO accountResponseDTO = accountServiceImplm.createAccount(requestDto);
+
+        assertNotNull(accountResponseDTO);
+        assertEquals(responseDTO.getAccountNumber(), account1.getAccountNumber()); //cannot use accountResponseDTO here
+        assertEquals(responseDTO.getSortCode(), accountResponseDTO.getSortCode());
+        assertEquals(responseDTO.getAccountName(), accountResponseDTO.getAccountName());
+        assertEquals(responseDTO.getBalance(), accountResponseDTO.getBalance());
+        assertEquals(responseDTO.getCustomerId(), accountResponseDTO.getCustomerId());
+
+        verify(customerRepository).findById(1L);
+        verify(accountRepository).save(any(Account.class));
 
     }
 
@@ -103,7 +115,8 @@ public class AccountServiceTest {
 //
 //
 //    }
-//
+
+}
 
 
 
